663c7c1366dd1f1213255bf8e651f93ca9628d99
Kazuho Oku <kazuhooku@gmail.com>
2012-03-29 18:31:17 +0900

    now successfully parses example/hello.jsx

--- example/hello.jsx
+++ example/hello.jsx
@@ -1,7 +1,7 @@
 package com.example.hello;
 
 class Hello {
-    static function main(args : String[]) : Int {
+    static function main(/* JSX does not yet support array typedefs args : String[] */) : Int {
         log "Hello, world!";
 		return 0;
     }
