ca0663d3133f6c3a7be79d460f2e87d8d9b861b2
Kazuho Oku <kazuhooku@gmail.com>
2012-03-30 17:29:09 +0900

    fix error (reference to undefined symbol) in example/fib.jsx

--- example/fib.jsx
+++ example/fib.jsx
@@ -5,9 +5,9 @@ class Fib {
 		if (n < 2)
 			return 1;
 		else
-			return fib(n - 1) + fib(n - 2);
+			return Fib.fib1(n - 1) + Fib.fib1(n - 2);
 	}
 	static function fib2(n : Int) : Int {
-		return n < 2 ? 1 : fib(n - 1) + fib(n - 2);
+		return n < 2 ? 1 : Fib.fib2(n - 1) + Fib.fib2(n - 2);
 	}
 }
