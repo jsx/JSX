fd457c24a4796a53973cf6b37c2eaa06f7229276
Kazuho Oku <kazuhooku@gmail.com>
2012-03-31 15:28:06 +0900

    implement for and for..in statement

--- example/fib.jsx
+++ example/fib.jsx
@@ -1,13 +1,28 @@
 package com.example.fib;
 
 class Fib {
+
 	static function fib1(n : Int) : Int {
 		if (n < 2)
 			return 1;
 		else
 			return Fib.fib1(n - 1) + Fib.fib1(n - 2);
 	}
+
 	static function fib2(n : Int) : Int {
 		return n < 2 ? 1 : Fib.fib2(n - 1) + Fib.fib2(n - 2);
+	}
+
+	static function fib3(n : Int) : Int {
+		if (n <= 2)
+			return 1;
+		var value = 1;
+		var prevValue = 1;
+		for (var i = 3; i <= n; i++) {
+			var t = value + prevValue;
+			prevValue = value;
+			value = t;
+		}
+		return value;
 	}
 }
