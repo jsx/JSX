c1faf38adae04a0c713bcf81c5eef564919b1fd6
Kazuho Oku <kazuhooku@gmail.com>
2012-04-04 23:10:51 +0900

    implement serialization

--- example/fib.jsx
+++ example/fib.jsx
@@ -7,7 +7,7 @@ class Fib {
 			return Fib.fib1(n - 1) + Fib.fib1(n - 2);
 	}
 
-	static function fib2(n : int) : int {
+	static function fib2(n : number) : number {
 		return n < 2 ? 1 : Fib.fib2(n - 1) + Fib.fib2(n - 2);
 	}
 
