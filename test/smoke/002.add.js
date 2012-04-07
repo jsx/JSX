8b7806f23efdfc020f923dc8c66e567db4503b53
Fuji, Goro <g.psy.va@gmail.com>
2012-04-08 02:35:59 +0900

    Add a test for example/add.jsx

---
+++ test/smoke/002.add.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+"use strict";
+
+var Test = require("Test");
+
+var Compiler = require("Compiler");
+
+var project_path = __dirname + "/../..";
+
+var test = new Test(__filename);
+
+test.describe('new Compiler', function(t) {
+    var compiler = new Compiler();
+
+    t.expect(compiler).toBeInstanceOf(Compiler);
+
+    t.done(compiler);
+}).next('compile', function(t, compiler) {
+    compiler.addSourceFile(project_path +
+                           "/example/add.jsx");
+
+    t.expect(compiler.compile()).toBe(true);
+
+    var source = compiler.getOutput();
+    t.expect(source, 'output').toBeTruthy();
+
+    var f = new Function(source);
+    t.expect(f).toBeInstanceOf(Function);
+
+    t.done();
+});
+
+test.done();
