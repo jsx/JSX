3ac3394919cf4221203c70b00dbe99ba7e0c98a0
Fuji, Goro <fuji.goro@dena.jp>
2012-04-07 23:16:45 +0900

    Add example/add.jsx

---
+++ example/add.jsx
@@ -0,0 +1,9 @@
+// example for declaration and binary expression
+class Add {
+    static function main(args : String[]) : int {
+        var x = 10;
+        var y = 20;
+        log x + y;
+        return 0;
+    }
+}
