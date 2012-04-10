8503a701abd176a093ef7d5e4489e1ae088f442f
Kazuho Oku <kazuhooku@gmail.com>
2012-04-10 18:31:28 +0900

    remove the comments from 003.binaryops.jsx so that it could be included in the test

---
+++ t/compile_and_run/003.binaryops.jsx
@@ -0,0 +1,20 @@
+/*EXPECTED
+8
+1.5
+3
+12
+4
+-1
+2147483647
+*/
+class Test {
+	static function run() : void {
+		log 2 * 4;
+		log 3 / 2;
+		log 8 % 5;
+		log 3 << 2;
+		log 9 >> 1;
+		log -1 >> 1;
+		log -1 >>> 1;
+	}
+}
