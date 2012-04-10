ab7c4a3028579aa999249bf4495d37fd63cb5c1d
Kazuho Oku <kazuhooku@gmail.com>
2012-04-11 06:41:35 +0900

    more tests

---
+++ t/compile_and_run/010.condop.jsx
@@ -0,0 +1,23 @@
+/*EXPECTED
+1
+1
+2
+3
+5
+8
+13
+*/
+class Test {
+	static function fib(n : number) : number {
+		return n <= 2 ? 1 : Test.fib(n - 1) + Test.fib(n - 2);
+	}
+	static function run() : void {
+		log Test.fib(1);
+		log Test.fib(2);
+		log Test.fib(3);
+		log Test.fib(4);
+		log Test.fib(5);
+		log Test.fib(6);
+		log Test.fib(7);
+	}
+}
