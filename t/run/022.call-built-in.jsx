dff333d6277739bfe1e6db0107f77891e86404f9
Kazuho Oku <kazuhooku@gmail.com>
2012-04-12 19:14:34 +0900

    autoboxing of String against a variable

--- t/run/022.call-built-in.jsx
+++ t/run/022.call-built-in.jsx
@@ -2,11 +2,16 @@
 hello
 5
 a
+abc
 */
 class Test {
 	static function run() : void {
+
 		log "hello".toString();
 		log "hello".length;
 		log String.fromCharCode(97);
+		var s = "abc";
+		log s.toString();
+
 	}
 }
