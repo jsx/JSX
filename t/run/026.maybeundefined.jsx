46358af5db9ba34905afabf43ed2ce945b68e4d7
Kazuho Oku <kazuhooku@gmail.com>
2012-04-15 12:38:44 +0900

    (undefined as MayBeUndefined<String>) + (undefined as MayBeUndefined<String>) should produce "undefinedundefined", not NaN

--- t/run/026.maybeundefined.jsx
+++ t/run/026.maybeundefined.jsx
@@ -1,12 +1,33 @@
 /*EXPECTED
 3
+NaN
 undefined
+NaN
+NaN
+abc
+abcundefined
+undefined
+undefinedundefined
+undefinedundefined
 */
 class Test {
 	static function run() : void {
 		var a : MayBeUndefined<number> = 3;
+		var b : MayBeUndefined<number> = undefined;
 		log a;
+		log a + b;
 		a = undefined;
 		log a;
+		log a + b;
+		log b + a;
+
+		var c : MayBeUndefined<String> = "abc";
+		var d : MayBeUndefined<String> = undefined;
+		log c;
+		log c + d;
+		c = undefined;
+		log c;
+		log c + d;
+		log d + c;
 	}
 }
