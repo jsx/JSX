ce84d496ac1459974eb8f54e674a8cecbe1a2066
Kazuho Oku <kazuhooku@gmail.com>
2012-04-17 21:02:21 +0900

    add hash impl.

---
+++ lib/built-in/Hash.jsx
@@ -0,0 +1,10 @@
+native final class Hash.<T> {
+
+	// 15.2.4
+	function toString() : String;
+	function toLocaleString() : String;
+	function hasOwnProperty(key : String) : boolean;
+
+}
+
+// vim: set noexpandtab:
