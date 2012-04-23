b90dc91b1e999b8024e3c266287bcd4eb5e140dd
Kazuho Oku <kazuhooku@gmail.com>
2012-04-23 13:35:07 +0900

    accept more than one class in one .jsx file

---
+++ t/run/033.many-class.jsx
@@ -0,0 +1,21 @@
+/*EXPECTED
+3
+3
+*/
+class A {
+	function initialize(a : int) {
+		this._a = a;
+	}
+	function get() : int {
+		return this._a;
+	}
+	var _a : int;
+}
+
+class Test {
+	static function run() : void {
+		var a = new A(3);
+		log a._a;
+		log a.get();
+	}
+}
