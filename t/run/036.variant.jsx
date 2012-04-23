f6f6a8232bdb55c131f4ced245b6882422cb67ec
Kazuho Oku <kazuhooku@gmail.com>
2012-04-23 17:03:31 +0900

    undefined should be part of MayBeUndefined

--- t/run/036.variant.jsx
+++ t/run/036.variant.jsx
@@ -1,19 +1,29 @@
 /*EXPECTED
+true
+false
+0
+NaN
+u,n,d,e,f,i,n,e,d
+
+false
 false
 0
 0
 f,a,l,s,e
 
+false
 true
 1
 1
 t,r,u,e
 
+false
 true
 1
 1.5
 1,.,5
 
+false
 true
 0
 NaN
@@ -21,25 +31,36 @@ abc
 */
 class Test {
 	static function run() : void {
-		var v : variant = false;
+		var v : variant = undefined;
+		log v == undefined;
+		log v as boolean;
+		log v as int;
+		log v as number;
+		log (v as String).split("").join(",");
+		log "";
+		v = false;
+		log v == undefined;
 		log v as boolean;
 		log v as int;
 		log v as number;
 		log (v as String).split("").join(",");
 		log "";
 		v = true;
+		log v == undefined;
 		log v as boolean;
 		log v as int;
 		log v as number;
 		log (v as String).split("").join(",");
 		log "";
 		v = 1.5;
+		log v == undefined;
 		log v as boolean;
 		log v as int;
 		log v as number;
 		log (v as String).split("").join(",");
 		log "";
 		v = "abc";
+		log v == undefined;
 		log v as boolean;
 		log v as int;
 		log v as number;
