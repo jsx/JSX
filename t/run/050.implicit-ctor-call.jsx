9355491334d441389771d2b610015c52033fce37
Kazuho Oku <kazuhooku@gmail.com>
2012-04-26 14:15:09 +0900

    make implicit calls to default constructors of base classes

---
+++ t/run/050.implicit-ctor-call.jsx
@@ -0,0 +1,22 @@
+/*EXPECTED
+C#initialize
+C#initialize
+M#initialize
+*/
+class C {
+	function initialize() {
+		log "C#initialize";
+	}
+}
+interface I {
+}
+mixin M {
+	function initialize() {
+		log "M#initialize";
+	}
+}
+class Test extends C implements I, M {
+	static function run() : void {
+		new Test();
+	}
+}
