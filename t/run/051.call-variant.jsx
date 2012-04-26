2ba3d9d8d12f7adeb9d253a2bdbed9d77dc36224
Kazuho Oku <kazuhooku@gmail.com>
2012-04-26 16:43:10 +0900

    add test for "as __nocheck__"

---
+++ t/run/051.call-variant.jsx
@@ -0,0 +1,17 @@
+/*EXPECTED
+4
+5
+*/
+class Test {
+	static function incr(n : number) : number {
+		return n + 1;
+	}
+	static function run() : void {
+		// call while casting
+		var f : variant = Test.incr;
+		log (f as __nocheck__ static function (:number):number)(3);
+		// assign and then call
+		var g : static function (:number):number = f as __nocheck__ static function (:number):number;
+		log g(4);
+	}
+}
