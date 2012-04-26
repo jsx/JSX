dd7cc2c2d433ee1fea7bd3de9325c4040b28c160
Kazuho Oku <kazuhooku@gmail.com>
2012-04-26 22:26:22 +0900

    add closure support

---
+++ t/run/052.lambda.jsx
@@ -0,0 +1,17 @@
+/*EXPECTED
+4
+5
+*/
+
+class Test {
+	static function lambda(x : number) : static function (: number) : number {
+		return function (y : number) : number {
+			return x + y;
+		};
+	}
+	static function run() : void {
+		var f = Test.lambda(3);
+		log f(1);
+		log f(2);
+	}
+}
---
+++ t/run/053.this-in-closure.jsx
@@ -0,0 +1,23 @@
+/*EXPECTED
+0
+1
+3
+*/
+
+class Test {
+	var n = 0;
+	function adder() : static function (: number) : void {
+		return function (x : number) : void {
+			this.n += x;
+		};
+	}
+	static function run() : void {
+		var t = new Test();
+		log t.n;
+		var adder = t.adder();
+		adder(1);
+		log t.n;
+		adder(2);
+		log t.n;
+	}
+}
