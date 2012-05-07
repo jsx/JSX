jsx/JSX

commit dc95bc4f96fac96e5d2ade9ffed28de37deb4c8a

Make sure "unexpected EOF" not to throw errors

src/util.js
@@ -189,7 +189,7 @@ var CompileError = exports.CompileError = Class.extend({
 		}
 
 		var content = compiler.getFileContent([] /* ignore errors */, null, this._filename);
-		var sourceLine = content.split(/^/m)[ this._lineNumber - 1 ];
+		var sourceLine = (content).split(/\n/)[ this._lineNumber - 1 ];
 
 		// fix visual width
 		var col = this._columnNumber;

t/compile_error/072.missing-last-brace.jsx
@@ -1,0 +1,6 @@
+class Test {
+	static function run() :void {
+		log "Hello, world!";
+	}
+// <- missing the last "}" here!
+

commit 305790d0892cf1b4fbf6cab737e75fb289df542a

Fix dc95bc4f9 which broke error messages (TODO: testing error messages)

src/util.js
@@ -189,7 +189,7 @@ var CompileError = exports.CompileError = Class.extend({
 		}
 
 		var content = compiler.getFileContent([] /* ignore errors */, null, this._filename);
-		var sourceLine = (content).split(/\n/)[ this._lineNumber - 1 ];
+		var sourceLine = content.split(/\n/)[ this._lineNumber - 1 ] + "\n";
 
 		// fix visual width
 		var col = this._columnNumber;