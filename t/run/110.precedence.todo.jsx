6377a704f4d7a617fcc3fcac21c26ca174d54426
Fuji, Goro <g.psy.va@gmail.com>
2012-05-14 00:18:16 +0900

    Add a failing test case about operator precedence of "as"

---
+++ t/run/110.precedence.todo.jsx
@@ -0,0 +1,15 @@
+/*EXPECTED
+x 100
+x 100
+x 100
+x 100
+*/
+
+class Test {
+	static function run() : void {
+		log "x ", (99+1) as string;
+		log "x " + ((99+1) as string);
+		log "x " +  (99+1) as string;
+		log "x " + (99+1).toString();
+	}
+}
