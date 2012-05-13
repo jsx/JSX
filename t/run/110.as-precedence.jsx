180ec512f93f4f90d37bbd8a4785a86e30be52c3
Kazuho Oku <kazuhooku@gmail.com>
2012-05-14 07:55:58 +0900

    fix bugs in recently added tests

--- t/run/110.as-precedence.jsx
+++ t/run/110.as-precedence.jsx
@@ -7,7 +7,7 @@ x 100
 
 class Test {
 	static function run() : void {
-		log "x ", (99+1) as string;
+		log "x", (99+1) as string;
 		log "x " + ((99+1) as string);
 		log "x " +  (99+1) as string;
 		log "x " + (99+1).toString();
--- t/run/111.comma-in-call-args.jsx
+++ t/run/111.comma-in-call-args.jsx
@@ -1,5 +1,5 @@
 /*EXPECTED
-2,4
+2 4
 */
 
 class Test {
