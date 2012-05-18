3a6acb16e3e2b5f51c0fba506e3963900c9dca3b
Fuji, Goro <g.psy.va@gmail.com>
2012-05-18 21:55:20 +0900

    Move a lib test to t/lib

--- t/lib/008.dom-interface.jsx
+++ t/lib/008.dom-interface.jsx
@@ -1,14 +1,11 @@
-/*EXPECTED
-ok
-*/
 // make sure the syntax of js/dom* is correct
-
+import "test-case.jsx";
 import "js/dom.jsx";
 import "js/dom/canvas2d.jsx";
 import "js/dom/webgl.jsx";
 
-class Test {
-	static function run() : void {
-		log "ok";
+class _Test extends TestCase {
+	function test_ok() : void {
+		this.expect(true).toBe(true);
 	}
 }
