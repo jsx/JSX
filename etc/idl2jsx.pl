e2a5d58ee9c79e87fc66d157eba24fbf398c778d
Fuji, Goro <g.psy.va@gmail.com>
2012-05-19 13:52:41 +0900

    Improve idl2jsx.pl

--- etc/idl2jsx.pl
+++ etc/idl2jsx.pl
@@ -4,43 +4,122 @@ use strict;
 use warnings;
 use Fatal qw(open);
 
+say "// generated from $_" for @ARGV;
+
+@ARGV = map {
+    /^https?:/ ? "w3m -dump $_ |" : $_;
+} @ARGV;
+
+
 my $content = do {
     local $/;
     <>;
 };
 
-my $rx_type = qr{
-    (?: \w+ (?: \s+ \w+)* )
-}xms;
+my %fake = (
+    Window => 1,
+    DocumentEvent => 1,
+);
 
+my %skip = (
+    EventListener => 1,
+);
+
+# NOTE: JSX's int is signed 32 bit integer
 my %typemap = (
-   DOMString => 'string',
+   'DOMString' => 'string',
+   'DOMString?' => 'String',
+   'DOMTimeStamp'=> 'number',
+   'byte'  => 'int',
+   'short' => 'int',
+   'long'  => 'int',
    'long long' => 'number',
-   byte  => 'int',
-   short => 'int',
-   long  => 'int',
    'unsigned byte' => 'int',
    'unsigned short' => 'int',
    'unsigned int' => 'int',
    'unsigned long' => 'int',
+   'unsigned long long' => 'number',
    'float' => 'number',
    'double' => 'number',
+
+   'any' => 'variant',
+
+   'EventListener' => 'function(:Event):void',
 );
 
-while($content =~ m{interface \s+ (?<class> \S+) \s+ (?: : \s+ (?<base> \S+) \s+) \{ (?<members> .+?) \}}xmsg) {
-    say "native class $+{class} extends $+{base} {";
+sub to_jsx_type {
+    my($idl_type) = @_;
+    $idl_type =~ s/.+://; # remove namespace
+    if(exists $typemap{$idl_type}) {
+        return $typemap{$idl_type} . "/*$idl_type*/";
+    }
+    else {
+        return $idl_type;
+    }
+}
+
+# the last ? means "nullable"
+my $rx_type = qr{
+    (?: [\:\w]+ (?: \s+ \w+)* [?]? )
+}xms;
+
+
+my %seen;
+
+while($content =~ m{interface \s+ (?<class> \S+) \s+ (?: : \s+ (?<base> \S+) \s+)? \{ (?<members> .+?) \}}xmsg) {
+    my $class = $+{class};
+
+    if($skip{$class}) {
+        next;
+    }
+
+    if($seen{$class}++) {
+        warn "# duplicated definition of $class, skipped.\n";
+        next;
+    }
+
+    my $classdecl = "native";
+    if($fake{$class}) {
+        $classdecl .= " __fake__";
+    }
+    $classdecl .= " class $class";
+
+    if($+{base}) {
+        $classdecl .= " extends $+{base}";
+    }
+
+    say "$classdecl {";
 
     foreach my $member(split /;/, $+{members}) {
-        $member =~ s{//[^\n]*}{}g;
-        if($member =~ m{ # member var
+        $member =~ s{(//[^\n]*)}{say "\t", $1; ""}ge; # remove comments
+        $member =~ s{\s+}{ }g; # compress extra spaces
+
+        if($member !~ /\S/) {
+            next;
+        }
+
+        # member var
+        if($member =~ m{
                 (?<readonly> readonly \s+)? attribute
-                \s+ (?<type> $rx_type) \s+ (?<ident> \S+)
+                \s+ (?<type> $rx_type) \s+ (?<ident> \w+)
             }xms) {
 
-            my $decl = $+{readonly} ? "const" : "var";
-            my $type = $typemap{$+{type}} // $+{type};
+            my $decl = "var";
+            if($+{readonly}) {
+                $decl = "__readonly__ $decl";
+            }
+            my $type = to_jsx_type($+{type});
             say "\t", "$decl $+{ident} : $type;";
         }
+        # member constant
+        elsif($member =~ m{
+                const \s+ (?<type> $rx_type) \s+ (?<ident> \w+)
+            }xms) {
+
+            my $type = to_jsx_type($+{type});
+            say "\t", "static const $+{ident} : $type;";
+        }
+        # member function
         elsif($member =~ m{
                 (?<ret_type> $rx_type)
                 \s+
@@ -50,15 +129,30 @@ while($content =~ m{interface \s+ (?<class> \S+) \s+ (?: : \s+ (?<base> \S+) \s+
                     (?<params> .*?)
                 \)
             }xms) { # member function
+
             my $ident = $+{ident};
-            my $ret_type= $typemap{$+{ret_type}} // $+{ret_type} ;
+            my $ret_type = to_jsx_type($+{ret_type});
 
             my $params = join ", ", map {
                 my($type, $id) = /(?: (?: in | optional ) \s+)* ($rx_type) \s+ (\w+) /xms
                     or die "Cannot parse line:  $_";
-                "$id : " . ($typemap{$type} // $type);
+                "$id : " . to_jsx_type($type);
             } split /,/, $+{params};
-            say "\t", "function $ident($params) : $ret_type;";
+
+
+            # prettify if needed
+            my $line = "\t" . "function $ident($params) : $ret_type;";
+            if(length $line > 70) {
+                $params =~ s/, \s+/,\n/xmsg;
+                $params =~ s/^/\t\t/xmsg;
+                $line = "\t" . "function $ident(\n";
+                $line .= $params . "\n";
+                $line .= "\t) : $ret_type;";
+            }
+            say $line;
+        }
+        else {
+            die "[BUG] canot parse member: $member\n";
         }
     }
 
