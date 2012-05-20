2fb1b7ba4953ca8df1d6b253db3e9333a9affb96
Fuji, Goro <g.psy.va@gmail.com>
2012-05-20 14:57:27 +0900

    Add compiling tests for dom.jsx

--- t/lib/008.dom-interface.jsx
+++ t/lib/008.dom-interface.jsx
@@ -5,6 +5,31 @@ import "js/dom/canvas2d.jsx";
 import "js/dom/webgl.jsx";
 
 class _Test extends TestCase {
+
+	function compile_check() : void {
+		var elem = dom.id("foo");
+		elem.innerHTML = "";
+		elem.addEventListener("click", function(e : Event) : void {
+			e.preventDefault();
+			e.stopPropagation();
+
+			var target = e.target as HTMLElement;
+			target.style.color = "red";
+		});
+
+		var canvas = dom.createCanvas();
+		var cx = canvas.getContext("2d");
+		assert cx != null;
+		log canvas.width;
+		log canvas.height;
+
+		var image = dom.createImage();
+		image.src = "bar.png";
+		log image.width;
+		log image.height;
+	}
+
+
 	function test_ok() : void {
 		this.expect(true).toBe(true);
 	}
