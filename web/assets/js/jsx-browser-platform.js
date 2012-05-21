6b55b5d448c31b8771dfabec913a78fc8c1c1547
Fuji, Goro <fuji.goro@dena.jp>
2012-05-21 16:29:08 +0900

    add Function#bind support for older versions of browsers

--- web/assets/js/jsx-browser-platform.js
+++ web/assets/js/jsx-browser-platform.js
@@ -1,3 +1,34 @@
+/* BrowserPlatform and workaround old browsers */
+
+// Function#bind is used only in the compiler,
+// so it is not required in bootstrap.js
+//
+// copied from MDN's implementation.
+// https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
+if (!Function.prototype.bind) {
+	Function.prototype.bind = function (oThis) {
+		if (typeof this !== "function") {
+			// closest thing possible to the ECMAScript 5 internal IsCallable function
+			throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+		}
+
+		var aArgs = Array.prototype.slice.call(arguments, 1), 
+				fToBind = this, 
+				fNOP = function () {},
+				fBound = function () {
+					return fToBind.apply(this instanceof fNOP
+															 ? this
+															 : oThis || window,
+															 aArgs.concat(Array.prototype.slice.call(arguments)));
+				};
+
+		fNOP.prototype = this.prototype;
+		fBound.prototype = new fNOP();
+
+		return fBound;
+	};
+}
+
 var BrowserPlatform = jsx.Platform.extend({
 	constructor: function(root) {
 		this._root = root;
