576c4a1753c05610371eb978f108312f49a27910
Fuji, Goro <g.psy.va@gmail.com>
2012-05-24 09:50:17 +0900

    Add failing test

---
+++ t/run/128.int-type-parameter.todo.jsx
@@ -0,0 +1,21 @@
+/*EXPECTED
+10
+20
+30
+*/
+
+class Test {
+	static function f(i : int) : void {
+		log i;
+	}
+
+	static function run()  : void {
+		Test.f(10.5);
+
+		var n = 20.5;
+		Test.f(n);
+
+		var mn : MayBeUndefined.<number> = 30.5;
+		Test.f(mn);
+	}
+}
