841930a3df68fb846693d63706cad7ace2674440
Fuji, Goro <g.psy.va@gmail.com>
2012-05-31 11:19:25 +0900

    No import in fib.jsx, to avoid failure in try-source-map.html

--- example/import.jsx
+++ example/import.jsx
@@ -1,8 +1,24 @@
 import "fib.jsx";
+import "test-case.jsx";
 
 class _Main {
 	static function main(args : string[]) : void {
 		log "fib(10)=" + Fib.fib1(10).toString();
+	}
+}
+
+class _Test extends TestCase {
+	function testFib1() : void {
+		this.expect(Fib.fib1(10)).toBe(55);
+	}
+	function testFib2() : void {
+		this.expect(Fib.fib2(10)).toBe(55);
+	}
+	function testFib3() : void {
+		this.expect(Fib.fib3(10)).toBe(55);
+	}
+	function testFib4() : void {
+		this.expect(Fib.fib4(10)).toBe(55);
 	}
 }
 
