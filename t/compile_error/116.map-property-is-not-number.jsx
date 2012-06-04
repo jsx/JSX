jsx/JSX

commit a9bb09d64029ffedc140673a53fa8c38e5c0ae04

support operators ".", "[]" against variant, to ease the pain of decoding JSON

src/expression.js
@@ -1081,8 +1081,8 @@ var PropertyExpression = exports.PropertyExpression = UnaryExpression.extend({
 			return false;
 		}
 		if (exprType.resolveIfMayBeUndefined().equals(Type.variantType)) {
-			context.errors.push(new CompileError(this._identifierToken, "cannot obtain a member of variant, use: (<<expr>> as Map.<type>)[<<name>>]"));
-			return false;
+			this._type = Type.variantType;
+			return true;
 		}
 		var classDef = exprType.getClassDef();
 		if (classDef == null) {
@@ -1319,11 +1319,18 @@ var ArrayExpression = exports.ArrayExpression = BinaryExpression.extend({
 		}
 		// obtain classDef
 		var expr1Type = this._expr1.getType().resolveIfMayBeUndefined();
-		if (! (expr1Type instanceof ObjectType)) {
-			context.errors.push(new CompileError(this._token, "cannot apply operator[] against a non-object"));
-			return false;
+		if (expr1Type instanceof ObjectType) {
+			return this._analyzeApplicationOnObject(expr1Type);
+		} else if (expr1Type.equals(Type.variantType)) {
+			return this._analyzeApplicationOnVariant();
+			return true;
 		}
-		var expr1ClassDef = expr1Type.getClassDef();;
+		context.errors.push(new CompileError(this._token, "cannot apply []; the operator is only applicable against an array or an variant"));
+		return false;
+	},
+
+	_analyzeApplicationOnObject: function (expr1Type) {
+		var expr1ClassDef = expr1Type.getClassDef();
 		// obtain type of operator []
 		var accessorType = expr1ClassDef.getMemberTypeByName("__native_index_operator__", false, ClassDefinition.GET_MEMBER_MODE_ALL);
 		if (accessorType == null) {
@@ -1345,6 +1352,16 @@ var ArrayExpression = exports.ArrayExpression = BinaryExpression.extend({
 		}
 		// set type of the expression
 		this._type = accessorType.getReturnType();
+		return true;
+	},
+
+	_analyzeApplicationOnVariant: function () {
+		var expr2Type = this._expr2.getType().resolveIfMayBeUndefined();
+		if (! (expr2Type.equals(Type.stringType) || expr2Type.isConvertibleTo(Type.numberType))) {
+			context.errors.push(new CompileError("the argument of variant[] should be a string or a number"));
+			return false;
+		}
+		this._type = Type.variantType;
 		return true;
 	},
 

t/compile_error/106.property-expr-on-variant.jsx
@@ -1,6 +1,0 @@
-class Test {
-	static function run() : void {
-		var v : variant = 0;
-		var g = v.a;
-	}
-}

t/run/137.variant-accessors.jsx
@@ -1,0 +1,36 @@
+/*EXPECTED
+1
+hello
+bar
+bar
+bar
+bar
+2
+hello
+world
+*/
+class Test {
+	static function run() : void {
+		var j : variant = {
+			n: 1,
+			s: "hello",
+			m: {
+				foo: "bar"
+			},
+			a: [
+				"hello",
+				"world"
+			]
+		} : Map.<variant>;
+		log j.n;
+		log j["s"];
+		log j.m.foo;
+		log j.m["foo"];
+		log j["m"].foo;
+		log j["m"]["foo"];
+		log j.a.length;
+		for (var i = 0; i < j.a.length as number; ++i) {
+			log j.a[i];
+		}
+	}
+}

t/run/138.json-complex.jsx
@@ -1,0 +1,26 @@
+/*EXPECTED
+1
+hello
+bar
+bar
+bar
+bar
+2
+hello
+world
+*/
+class Test {
+	static function run() : void {
+		var j = JSON.parse('{"n":1,"s":"hello","m":{"foo":"bar"},"a":["hello","world"]}');
+		log j.n;
+		log j["s"];
+		log j.m.foo;
+		log j.m["foo"];
+		log j["m"].foo;
+		log j["m"]["foo"];
+		log j.a.length;
+		for (var i = 0; i < j.a.length as number; ++i) {
+			log j.a[i];
+		}
+	}
+}

commit 94c41505ae6f258cb5b164a18fb903fbaee5c9b0

fix compiler crash on syntax error, introduced in commit a9bb09d

src/expression.js
@@ -1320,16 +1320,16 @@ var ArrayExpression = exports.ArrayExpression = BinaryExpression.extend({
 		// obtain classDef
 		var expr1Type = this._expr1.getType().resolveIfMayBeUndefined();
 		if (expr1Type instanceof ObjectType) {
-			return this._analyzeApplicationOnObject(expr1Type);
+			return this._analyzeApplicationOnObject(context, expr1Type);
 		} else if (expr1Type.equals(Type.variantType)) {
-			return this._analyzeApplicationOnVariant();
+			return this._analyzeApplicationOnVariant(context);
 			return true;
 		}
 		context.errors.push(new CompileError(this._token, "cannot apply []; the operator is only applicable against an array or an variant"));
 		return false;
 	},
 
-	_analyzeApplicationOnObject: function (expr1Type) {
+	_analyzeApplicationOnObject: function (context, expr1Type) {
 		var expr1ClassDef = expr1Type.getClassDef();
 		// obtain type of operator []
 		var accessorType = expr1ClassDef.getMemberTypeByName("__native_index_operator__", false, ClassDefinition.GET_MEMBER_MODE_ALL);
@@ -1347,7 +1347,7 @@ var ArrayExpression = exports.ArrayExpression = BinaryExpression.extend({
 		}
 		// check type of expr2
 		if (! this._expr2.getType().isConvertibleTo(accessorType.getArgumentTypes()[0])) {
-			context.errors.push(new CompileError(this._token, "index type is incompatible (expected '" + accessorType.getArgumentTypes()[0].toString() + "', got '" + this._expr2.getType().toString() + "'"));
+			context.errors.push(new CompileError(this._token, "index type is incompatible (expected '" + accessorType.getArgumentTypes()[0].toString() + "', got '" + this._expr2.getType().toString() + "')"));
 			return false;
 		}
 		// set type of the expression
@@ -1355,7 +1355,7 @@ var ArrayExpression = exports.ArrayExpression = BinaryExpression.extend({
 		return true;
 	},
 
-	_analyzeApplicationOnVariant: function () {
+	_analyzeApplicationOnVariant: function (context) {
 		var expr2Type = this._expr2.getType().resolveIfMayBeUndefined();
 		if (! (expr2Type.equals(Type.stringType) || expr2Type.isConvertibleTo(Type.numberType))) {
 			context.errors.push(new CompileError("the argument of variant[] should be a string or a number"));

t/compile_error/116.map-property-is-not-number.jsx
@@ -1,0 +1,6 @@
+class Test {
+	static function run() : void {
+		var m = { a: 1 };
+		m[0];
+	}
+}