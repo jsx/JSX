cfa45aac76e9b20388d3f0b870515597c6a78fef
Kazuho Oku <kazuhooku@gmail.com>
2012-06-05 22:57:45 +0900

    add template inline expansion test

---
+++ t/optimize/025.inline-static-template-function.jsx
@@ -0,0 +1,22 @@
+/*EXPECTED
+4
+abcdef
+*/
+/*JSX_OPTS
+--optimize inline,fold-const
+*/
+class Adder.<T> {
+	static function f(x : T, y : T) : T {
+		var r = x + y;
+		return r;
+	}
+}
+
+class Test {
+	static function run() : void {
+		var n = Adder.<number>.f(1, 3);
+		log n;
+		var s = Adder.<string>.f("abc", "def");
+		log s;
+	}
+}
