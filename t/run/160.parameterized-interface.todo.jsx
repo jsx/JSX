18f1badbe6dd60aabd3112fa28388a89a2559618
Fuji, Goro <g.psy.va@gmail.com>
2012-06-13 09:36:00 +0900

    add a failing test for parameterized interfaces

---
+++ t/run/160.parameterized-interface.todo.jsx
@@ -0,0 +1,17 @@
+
+interface I.<T> {
+	function f() : T;
+}
+
+class C implements I.<string> {
+	override function f() : string {
+		return "foo";
+	}
+}
+
+class Test {
+	static function run() : void {
+		var i : I.<string> = new C();
+		log i.f();
+	}
+}
