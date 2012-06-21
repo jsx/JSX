8fb1eaaca9afcfd0621d092ab6ebec45fa7b51de
Fuji, Goro <g.psy.va@gmail.com>
2012-06-21 10:04:20 +0900

    fix BroowserPlatform#_findPath()

--- web/src/browser-platform.js
+++ web/src/browser-platform.js
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2012 DeNA Co., Ltd.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
  * deal in the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
  * sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -36,8 +36,8 @@ if (!Function.prototype.bind) {
 			throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
 		}
 
-		var aArgs = Array.prototype.slice.call(arguments, 1), 
-				fToBind = this, 
+		var aArgs = Array.prototype.slice.call(arguments, 1),
+				fToBind = this,
 				fNOP = function () {},
 				fBound = function () {
 					return fToBind.apply(this instanceof fNOP
@@ -53,6 +53,8 @@ if (!Function.prototype.bind) {
 	};
 }
 
+var debug = (location.hash === "#debug");
+
 var BrowserPlatform = exports.BrowserPlatform = Platform.extend({
 	constructor: function(root) {
 		this._root = root;
@@ -60,7 +62,7 @@ var BrowserPlatform = exports.BrowserPlatform = Platform.extend({
 		this._content = {};
 
 		this._map = JSON.parse(this.load(root + "/tree.generated.json"));
-		
+
 		this._prefix = document.location.pathname.replace(/\/[^\/]+$/, "");
 	},
 
@@ -69,9 +71,11 @@ var BrowserPlatform = exports.BrowserPlatform = Platform.extend({
 	},
 
 	_findPath: function (path) {
-		path = (this._prefix + "/" + path).replace(/[^\/]+\/\.\.\//g, "");
+		var absPath = (this._prefix + "/" + path).
+			replace(/[^\/]+\/\.\.\//g, "").
+			replace(/\/\/+/g, "/");
 
-		var parts = path.split('/');
+		var parts = absPath.split('/');
 		var cur = this._map;
 		while(parts.length > 0) {
 			var t = cur[parts.shift()];
@@ -80,6 +84,12 @@ var BrowserPlatform = exports.BrowserPlatform = Platform.extend({
 			}
 			cur = t;
 		}
+
+		if(debug) {
+			console.debug("[D] find path=%s (absPath=%s) -> %s",
+					path, absPath, JSON.stringify(cur));
+		}
+
 		return cur;
 	},
 
