d21ef72185d9b5af0b12e070b033a7653f386e24
mzp <mzpppp@gmail.com>
2012-07-01 15:43:46 +0900

    fix array length optimizer bug

--- t/optimize/027.array-length.jsx
+++ t/optimize/027.array-length.jsx
@@ -8,7 +8,7 @@ class Test {
 	static function run() : void {
 		var a = [ 1, 2, 3, 4 ];
 		var sum = 0;
-		for (var i = 0; i < a.length; ++i) {
+		for (var i = 0; i < 1*a.length; ++i) {
 			sum += a[i];
 		}
 		log sum;
