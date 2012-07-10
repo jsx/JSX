e07cfdd5a8b08440f9a147cd88747f40d93920f4
Kazuho Oku <kazuhooku@gmail.com>
2012-07-11 05:30:33 +0900

    do not raise compile error for: null as __noconvert__ Nullable.<T>

--- src/expression.js
+++ src/expression.js
@@ -946,7 +946,7 @@ var AsNoConvertExpression = exports.AsNoConvertExpression = UnaryExpression.exte
 		if (! this._analyze(context))
 			return false;
 		var srcType = this._expr.getType();
-		if ((srcType.equals(Type.nullType) && ! (this._type instanceof ObjectType || this._type instanceof FunctionType))) {
+		if ((srcType.equals(Type.nullType) && ! (this._type instanceof NullableType || this._type instanceof ObjectType || this._type instanceof FunctionType))) {
 			context.errors.push(new CompileError(this._token, "'" + srcType.toString() + "' cannot be treated as a value of type '" + this._type.toString() + "'"));
 			return false;
 		}
