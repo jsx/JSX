4e8583aeee2db0d7cf9b1cf275929b0c0e3bbed6
Fuji, Goro <g.psy.va@gmail.com>
2012-07-18 00:40:41 +0900

    fix symbol names to make error messages more readable

--- lib/js/timer.jsx
+++ lib/js/timer.jsx
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2012 DeNA Co., Ltd.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
  * deal in the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
  * sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,22 +28,22 @@ final class TimerHandle {
 
 final class Timer {
 	static function setTimeout(listener : function():void, milliseconds :int) : TimerHandle {
-		var f = js.global["setTimeout"] as function(:function():void, :int): TimerHandle;
-		return f(listener, milliseconds);
+		var setTimeout = js.global["setTimeout"] as function(:function():void, :int): TimerHandle;
+		return setTimeout(listener, milliseconds);
 	}
 
 	static function clearTimeout(timerID : TimerHandle) :void {
-		var f = js.global["clearTimeout"] as function(:TimerHandle):void;
-		f(timerID);
+		var clearTimeout = js.global["clearTimeout"] as function(:TimerHandle):void;
+		clearTimeout(timerID);
 	}
 
 	static function setInterval(listener : function():void, milliseconds :int) : TimerHandle {
-		var f = js.global["setInterval"] as function(:function():void, :int): TimerHandle;
-		return f(listener, milliseconds);
+		var setInterval = js.global["setInterval"] as function(:function():void, :int): TimerHandle;
+		return setInterval(listener, milliseconds);
 	}
 
 	static function clearInterval(timerID : TimerHandle) :void {
-		var f = js.global["clearInterval"] as function(:TimerHandle):void;
-		f(timerID);
+		var clearInterval = js.global["clearInterval"] as function(:TimerHandle):void;
+		clearInterval(timerID);
 	}
 }
