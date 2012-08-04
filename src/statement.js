108ba61380e942ef2c0955554f2f9cb96e4d5b55
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2012-08-04 13:56:14 +0900

    Add alternate constructor invocation

--- src/statement.js
+++ src/statement.js
@@ -165,6 +165,82 @@ var ConstructorInvocationStatement = exports.ConstructorInvocationStatement = St
 
 });
 
+var AlternateConstructorInvocationStatement = exports.AlternateConstructorInvocationStatement = Statement.extend({
+
+	constructor: function (thisExpression, args) {
+		Statement.prototype.constructor.call(this);
+		this._thisExpression = thisExpression;
+		this._args = args;
+		this._ctorFunctionType = null;
+	},
+
+	clone: function () {
+		return new AlternateConstructorInvocationStatement(
+			this._thisExpression, Util.cloneArray(this._args));
+	},
+
+	getToken: function () {
+		return this._thisExpression.getToken();
+	},
+
+	getArguments: function () {
+		return this._args;
+	},
+
+	getConstructingClassDef: function() {
+		return this._thisExpression._classDef;
+	},
+
+	getConstructorType: function () {
+		return this._ctorFunctionType;
+	},
+
+	serialize: function () {
+		return [
+			"AlternateConstructorInvocationStatement",
+			this._thisExpression.serialize(),
+			Util.serializeArray(this._args)
+		];
+	},
+
+	doAnalyze: function (context) {
+		if (! this._thisExpression.analyze(context, null)) {
+			return false;
+		}
+
+		var ctorType = this._thisExpression._classDef.getMemberTypeByName("constructor", false, ClassDefinition.GET_MEMBER_MODE_CLASS_ONLY);
+		if (ctorType == null) {
+			if (this._args.length != 0) {
+				context.errors.push(new CompileError(this.getToken().getToken(), "no function with matching arguments"));
+				return true;
+			}
+			ctorType = new ResolvedFunctionType(Type.voidType, [], false); // implicit constructor
+		} else {
+			// analyze args
+			var argTypes = Util.analyzeArgs(
+				context, this._args, null,
+				ctorType.getExpectedCallbackTypes(this._args.length, false));
+			if (argTypes == null) {
+				// error is reported by callee
+				return true;
+			}
+			if ((ctorType = ctorType.deduceByArgumentTypes(context, this.getToken(), argTypes, false)) == null) {
+				// error is reported by callee
+				return true;
+			}
+		}
+		this._ctorFunctionType = ctorType;
+		return true;
+	},
+
+	forEachExpression: function (cb) {
+		if (! Util.forEachExpression(cb, this._args))
+			return false;
+		return true;
+	}
+
+});
+
 // statements that take one expression
 
 var UnaryExpressionStatement = exports.UnaryExpressionStatement = Statement.extend({
