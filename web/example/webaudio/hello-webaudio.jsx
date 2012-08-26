4e756d18b82993b334ff2013921d378434e0b82a
Fuji, Goro <g.psy.va@gmail.com>
2012-08-26 12:49:00 +0900

    refactor WebAudio example

--- web/example/webaudio/hello-webaudio.jsx
+++ web/example/webaudio/hello-webaudio.jsx
@@ -1,91 +1,91 @@
 import 'js/web.jsx';
 
 class _Main {
-	static function main(args : string[]) : void {
-		try {
-			_Main.init(new webkitAudioContext);
-		}
-		catch (e : Error) {
-			log e;
-		}
-	}
+    static function main(args : string[]) : void {
+        try {
+            _Main.init(new webkitAudioContext);
+        }
+        catch (e : Error) {
+            log e;
+        }
+    }
 
-	static function init(cx : AudioContext) : void {
-		var app = new App(cx);
+    static function init(cx : AudioContext) : void {
+        var app = new App(cx);
 
-		app.init();
-	}
+        app.init();
+    }
 }
 
 class App {
-	var audioFilename = "amairo.mp3";
+    var audioFilename = "amairo.mp3";
 
-	var cx : AudioContext;
-	var source : AudioBufferSourceNode;
-	var buffer : AudioBuffer = null;
+    var cx : AudioContext;
+    var source : AudioBufferSourceNode;
 
-	function constructor(cx : AudioContext) {
-		log cx;
-		this.cx = cx;
-	}
+    function constructor(cx : AudioContext) {
+        this.cx = cx;
+    }
 
-	function setMessage(text : string) : void {
-		var element = dom.id("message");
+    function setMessage(text : string) : void {
+        var element = dom.id("message");
 
-		var children = element.childNodes;
-		for (var i = 0, l = children.length; i < l; ++i) {
-			element.removeChild(children[i]);
-		}
+        var children = element.childNodes;
+        for (var i = 0, l = children.length; i < l; ++i) {
+            element.removeChild(children[i]);
+        }
 
-		element.appendChild(dom.document.createTextNode(text));
-	}
+        element.appendChild(dom.document.createTextNode(text));
+    }
 
-	function init() : void {
-		this.setMessage("WebAudio API is supported on this browser.");
+    function init() : void {
+        this.setMessage("WebAudio API is supported on this browser.");
 
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', this.audioFilename);
-		xhr.responseType = "arraybuffer";
-		xhr.addEventListener("load", (e) -> {
-			this.cx.decodeAudioData(xhr.response as ArrayBuffer, (buffer) -> {
-				this.buffer = buffer;
-				this.readyToStart();
-			}, (buffer) -> {
-				this.setMessage("Loading error.");
-			});
-		});
-		xhr.send();
-	}
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', this.audioFilename);
+        xhr.responseType = "arraybuffer";
+        xhr.addEventListener("load", (e) -> {
+            this.cx.decodeAudioData(xhr.response as ArrayBuffer, (buffer) -> {
+                this.readyToStart(buffer);
+            }, (buffer) -> {
+                this.setMessage("Loading error.");
+            });
+        });
+        xhr.send();
+    }
 
-	function readyToStart() : void {
-		(dom.id("start") as HTMLInputElement).disabled = false;
-		(dom.id("stop")  as HTMLInputElement).disabled = false;
+    function readyToStart(buffer : AudioBuffer) : void {
+        var start = dom.id("start") as __noconvert__ HTMLInputElement;
+        var stop  = dom.id("stop")  as __noconvert__ HTMLInputElement;
 
-		dom.id("start").addEventListener("click", (e) -> {
-			this.start();
-		});
-		dom.id("stop").addEventListener("click", (e) -> {
-			this.stop();
-		});
+        start.disabled = false;
+        stop.disabled  = false;
 
-	}
+        start.addEventListener("click", (e) -> {
+            this.start(buffer);
+        });
+        stop.addEventListener("click", (e) -> {
+            this.stop();
+        });
 
-	function start() : void {
-		log "start";
+    }
 
-		var source = this.cx.createBufferSource();
-		source.buffer = this.buffer;
-		source.connect(this.cx.destination);
-		source.noteOn(0);
+    function start(buffer : AudioBuffer) : void {
+        log "start";
 
-		this.source = source;
-	}
+        var source = this.cx.createBufferSource();
+        source.buffer = buffer;
+        source.connect(this.cx.destination);
+        source.noteOn(0);
 
-	function stop() : void {
-		log "stop";
+        this.source = source;
+    }
 
-		this.source.noteOff(0);
-	}
+    function stop() : void {
+        log "stop";
+
+        this.source.noteOff(0);
+    }
 }
 
-
+// vim: set expandtab:
