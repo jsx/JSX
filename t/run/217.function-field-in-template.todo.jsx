141d799b9216b2c2c57e5cf35a45a7dd8d1a8ffc
Fuji, Goro <g.psy.va@gmail.com>
2012-10-14 19:10:01 +0900

    cleanup TODO tests
    the problem is that function fields in template are not
    instantiated.

--- t/run/217.function-field-in-template.todo.jsx
+++ t/run/217.function-field-in-template.todo.jsx
@@ -3,13 +3,13 @@
 */
 
 class C.<T> {
-	static const toT = function (a : Nullable.<T>) : T {
+	static const f = function (a : T) : T {
 		return a;
 	};
 }
 
 class _Main {
 	static function main(args : string[]) : void {
-		log C.<number>.toT(10);
+		log C.<number>.f(10);
 	}
 }
