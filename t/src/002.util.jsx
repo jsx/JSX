d72d1bd6ebc6124029d7d9d5e13d9727f1e3d1d2
Yuichi Nishiwaki <nishiwaki.yuichi@dena.jp>
2012-12-21 22:08:51 +0900

    update parser tests

---
+++ t/src/002.util.jsx
@@ -0,0 +1,58 @@
+// JSX_TEST
+import "test-case.jsx";
+import "../../src/util.jsx";
+
+class _Test extends TestCase {
+	function testFormat() : void {
+		this.expect( Util.format("foo", new string[]) ).toBe("foo");
+		this.expect( Util.format("foo %1", ["bar"])).toBe("foo bar");
+		this.expect( Util.format("foo %1 %1", ["bar"])).toBe("foo bar bar");
+		this.expect( Util.format("foo %2 %1", ["bar", "baz"]) ).toBe('foo baz bar');
+		this.expect( Util.format("100%%1 %1", ["foo"]) ).toBe("100%1 foo");
+	}
+
+	function testRepeat() : void {
+		this.expect( Util.repeat("x", 4) ).toBe("xxxx");
+		this.expect( Util.repeat("ab", 4) ).toBe("abababab");
+		this.expect( Util.repeat("z", 0) ).toBe("");
+	}
+
+	function testEncodeStringLiteral() : void {
+		this.expect(Util.encodeStringLiteral("")).toBe('""');
+		this.expect(Util.encodeStringLiteral("abc")).toBe('"abc"');
+		this.expect(Util.encodeStringLiteral('"')).toBe('"\\""');
+		this.expect(Util.encodeStringLiteral('\0')).toBe('"\\0"');
+		this.expect(Util.encodeStringLiteral('\\')).toBe('"\\\\"');
+		this.expect(Util.encodeStringLiteral('\u0345')).toBe('"\\u0345"');
+	}
+
+	function testDecodeStringLiteral() : void {
+		this.expect(Util.decodeStringLiteral("''")).toBe("");
+		this.expect(Util.decodeStringLiteral('""')).toBe("");
+		this.expect(Util.decodeStringLiteral("'abc'")).toBe("abc");
+		this.expect(Util.decodeStringLiteral("'\\''")).toBe("'");
+		this.expect(Util.decodeStringLiteral('"\\""')).toBe('"');
+		this.expect(Util.decodeStringLiteral("'\\\\'")).toBe("\\");
+		this.expect(Util.decodeStringLiteral("'\\b'")).toBe("\b");
+		this.expect(Util.decodeStringLiteral("'\\f'")).toBe("\f");
+		this.expect(Util.decodeStringLiteral("'\\n'")).toBe("\n");
+		this.expect(Util.decodeStringLiteral("'\\t'")).toBe("\t");
+		this.expect(Util.decodeStringLiteral("'\\v'")).toBe("\v");
+		this.expect(Util.decodeStringLiteral("'\\u0041'")).toBe("A");
+		this.expect(Util.decodeStringLiteral("'\\0'")).toBe("\0");
+		this.expect(Util.decodeStringLiteral("'!\\u0041!\\0!\\n!'")).toBe("!A!\0!\n!");
+	}
+
+	function testResolvePath() : void {
+		this.expect(Util.resolvePath("a/b/c")).toBe("a/b/c");
+		this.expect(Util.resolvePath("a/./b")).toBe("a/b");
+		this.expect(Util.resolvePath("a/../b")).toBe("b");
+		this.expect(Util.resolvePath("a/../../b")).toBe("../b");
+		this.expect(Util.resolvePath("../../a")).toBe("../../a");
+		this.expect(Util.resolvePath("/a/b/c")).toBe("/a/b/c");
+		this.expect(Util.resolvePath("/a/../b")).toBe("/b");
+		this.expect(Util.resolvePath("/a/../../c")).toBe("/c");
+	}
+
+}
+
---
+++ t/src/003.source-map.jsx
@@ -0,0 +1,76 @@
+import "js.jsx";
+import "test-case.jsx";
+import "js/nodejs.jsx";
+import "../../src/util.jsx";
+import "../util/jslexer.jsx";
+
+class _Test extends TestCase {
+
+	function testWithSourceMapConsumer() : void {
+		var eval = js.global['eval'] as (string) -> variant;
+
+		function search(a : JSToken[], predicate : (JSToken) -> boolean) : number {
+			for(var i = 0; i < a.length; ++i) {
+				if(predicate(a[i])) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		if(process.env["JSX_DISABLE_SOURCE_MAP_TEST"]) {
+			return;
+		}
+
+		node.child_process.execFile("bin/jsx", ["--enable-source-map", "--output", "t/source-map/hello.jsx.js", "t/source-map/hello.jsx"], {} : variant, function (code, stdout, stderr) {
+			this.expect(code, "error code").toBe(null);
+			this.expect(stderr, "stderr").toBe("");
+			this.expect(stdout, "stdout").toBe("");
+
+			if(code != null) {
+				return;
+			}
+
+			var source = JSLexer.tokenize("hello.jsx.js",
+				node.fs.readFileSync("t/source-map/hello.jsx.js").toString());
+
+			var mapping = JSON.parse(node.fs.readFileSync("t/source-map/hello.jsx.js.mapping").toString());
+
+			this.expect(mapping['file'], "mapping.file").toBe("t/source-map/hello.jsx.js");
+			var sources = ["t/source-map/hello.jsx", "lib/js/timer.jsx", "lib/js/js.jsx"].sort();
+			this.expect(JSON.stringify((mapping['sources'] as string[]).sort()), "mapping.sources").toBe(JSON.stringify(sources));
+
+			var SourceMapConsumer = eval('require("source-map").SourceMapConsumer');
+			var consumer = eval('new SourceMapConsumer(mapping)');
+
+			var pos, orig;
+
+			pos = search(source, function (t) { return t.token == '"Hello, world!"'; });
+			this.note("generated (literal)" + JSON.stringify(source[pos]));
+			orig = eval('consumer.originalPositionFor(source[pos])');
+			this.note("original: " + JSON.stringify(orig));
+			this.expect(orig['line'], "orig.line").toBe(14);
+			this.expect(orig['column'], "orig.column").toBe(12);
+			this.expect(orig['name'], "orig.name").toBe(null);
+
+			pos = search(source, function (t) { return t.token == '_Main'; });
+			this.note("generated (class): " + JSON.stringify(source[pos]));
+			orig = eval('consumer.originalPositionFor(source[pos])');
+			this.note("original: " + JSON.stringify(orig));
+			this.expect(orig['line'], "orig.line").toBe(5);
+			this.expect(orig['column'], "orig.column").toBe(6);
+			this.expect(orig['name'], "orig.name").toBe("_Main");
+
+			pos = search(source, function (t) { return t.token == 'getFoo$'; });
+			this.note("generated (member function): " + JSON.stringify(source[pos]));
+			orig = eval('consumer.originalPositionFor(source[pos])');
+			this.note("original: " + JSON.stringify(orig));
+			this.expect(orig['line'], "orig.line").toBe(9);
+			this.expect(orig['column'], "orig.column").toBe(13);
+			this.expect(orig['name'], "orig.name").toBe("getFoo");
+		});
+	}
+
+}
+
+// vim: set noexpandtab:
