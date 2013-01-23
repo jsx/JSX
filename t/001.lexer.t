c868d2976e3fd04821e9c75aa8484bbbe3eb0ca6
Fuji, Goro <g.psy.va@gmail.com>
2013-01-23 09:43:35 +0900

    cleanup test files

--- t/001.lexer.t
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-use Test::More tests => 1;
-
-my $file = 't/src/001.lexer.jsx';
-my $src = `bin/jsx --test $file`;
-is $?, 0, "bin/jsx --test $file";
-exit;
--- t/src/003.source-map.jsx
+++ t/src/003.source-map.jsx
@@ -1,14 +1,18 @@
 import "js.jsx";
-import "test-case.jsx";
 import "js/nodejs.jsx";
+
+import "test-case.jsx";
+
 import "../../src/util.jsx";
 import "../util/jslexer.jsx";
+
+native __fake__ class SourceMapConsumer {
+	function originalPositionFor(generatedPos : variant) : variant;
+}
 
 class _Test extends TestCase {
 
 	function testWithSourceMapConsumer() : void {
-		var eval = js.global['eval'] as (string) -> variant;
-
 		function search(a : JSToken[], predicate : (JSToken) -> boolean) : number {
 			for(var i = 0; i < a.length; ++i) {
 				if(predicate(a[i])) {
@@ -22,6 +26,8 @@ class _Test extends TestCase {
 			return;
 		}
 
+		var cwd = process.cwd();
+
 		node.child_process.execFile("bin/jsx", ["--enable-source-map", "--output", "t/source-map/hello.jsx.js", "t/source-map/hello.jsx"], {} : variant, function (code, stdout, stderr) {
 			this.expect(code, "error code").toBe(null);
 			this.expect(stderr, "stderr").toBe("");
@@ -31,6 +37,8 @@ class _Test extends TestCase {
 				return;
 			}
 
+			module.paths.push(cwd + "/node_modules");
+
 			var source = JSLexer.tokenize("hello.jsx.js",
 				node.fs.readFileSync("t/source-map/hello.jsx.js").toString());
 
@@ -39,31 +47,32 @@ class _Test extends TestCase {
 			this.expect(mapping['file'], "mapping.file").toBe("t/source-map/hello.jsx.js");
 			var sources = ["t/source-map/hello.jsx", "lib/js/timer.jsx", "lib/js/js.jsx"].sort();
 			this.expect(JSON.stringify((mapping['sources'] as string[]).sort()), "mapping.sources").toBe(JSON.stringify(sources));
-
-			var SourceMapConsumer = eval('require("source-map").SourceMapConsumer');
-			var consumer = eval('new SourceMapConsumer(mapping)');
+			var consumer = js.execScript('new (require("source-map").SourceMapConsumer)(mapping)') as __noconvert__ SourceMapConsumer;
 
 			var pos, orig;
 
+			this.note('search for "Hello, world!"');
 			pos = search(source, function (t) { return t.token == '"Hello, world!"'; });
 			this.note("generated (literal)" + JSON.stringify(source[pos]));
-			orig = eval('consumer.originalPositionFor(source[pos])');
+			orig = consumer.originalPositionFor(source[pos]);
 			this.note("original: " + JSON.stringify(orig));
 			this.expect(orig['line'], "orig.line").toBe(14);
 			this.expect(orig['column'], "orig.column").toBe(12);
 			this.expect(orig['name'], "orig.name").toBe(null);
 
-			pos = search(source, function (t) { return t.token == '_Main'; });
+			this.note('search for _Main');
+			pos = search(source, function (t) { return /^_Main\b/.test(t.token); });
 			this.note("generated (class): " + JSON.stringify(source[pos]));
-			orig = eval('consumer.originalPositionFor(source[pos])');
+			orig = consumer.originalPositionFor(source[pos]);
 			this.note("original: " + JSON.stringify(orig));
 			this.expect(orig['line'], "orig.line").toBe(5);
 			this.expect(orig['column'], "orig.column").toBe(6);
 			this.expect(orig['name'], "orig.name").toBe("_Main");
 
-			pos = search(source, function (t) { return t.token == 'getFoo$'; });
+			this.note('search for getFoo$');
+			pos = search(source, function (t) { return /^getFoo\b/.test(t.token); });
 			this.note("generated (member function): " + JSON.stringify(source[pos]));
-			orig = eval('consumer.originalPositionFor(source[pos])');
+			orig = consumer.originalPositionFor(source[pos]);
 			this.note("original: " + JSON.stringify(orig));
 			this.expect(orig['line'], "orig.line").toBe(9);
 			this.expect(orig['column'], "orig.column").toBe(13);
@@ -73,4 +82,4 @@ class _Test extends TestCase {
 
 }
 
-// vim: set noexpandtab:
+// vim: set noexpandtab ft=jsx:
