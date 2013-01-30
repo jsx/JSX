1a8b01b71633517541daa97862da204a2005c8aa
Fuji, Goro <g.psy.va@gmail.com>
2013-01-30 11:14:28 +0900

    remove duplicated files

--- t/compile_error/123.compiler-note.jsx
+++
@@ -1,8 +0,0 @@
-class _Main {
-  static function a (x : number) : void {}
-    static function a (x : string) : void {}
-    static function a (x : number, y : string) : void {}
-    static function main(args : string[]) : void {
-        _Main.a([1]);
-    }
-}
