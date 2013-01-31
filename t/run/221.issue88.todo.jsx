8a888213ac1c6ee70678a151f6a4c3f6ea8e8227
Fuji, Goro <g.psy.va@gmail.com>
2013-01-31 10:40:35 +0900

    add a TODO file related to #88

---
+++ t/run/221.issue88.todo.jsx
@@ -0,0 +1,13 @@
+// https://github.com/jsx/JSX/issues/88
+/*EXPECTED
+false
+*/
+
+class _Main {
+  static function main(args : string[]) : void {
+    log true == (1 != 1);
+  }
+}
+
+// vim: set tabstop=2 shiftwidth=2 expandtab:
+
