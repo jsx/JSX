a9ef6a0d75dc264324523b49682a92f772444fe5
Fuji, Goro <g.psy.va@gmail.com>
2013-02-03 01:04:33 +0900

    add example to WebRTC

---
+++ web/example/webrtc/app.jsx
@@ -0,0 +1,19 @@
+// works on only Chrome (as of 2013/2)
+import "js/web.jsx";
+
+class _Main {
+	static function main(args : string[]) : void {
+		log "application start";
+
+		// WebRTC video
+		dom.window.navigator.webkitGetUserMedia(
+			{ video : true } : Map.<variant>,
+			function (stream) {
+				log "MediaStream start";
+				var v = dom.id("video") as HTMLVideoElement;
+				v.src = URL.createObjectURL(stream);
+			}, function (error) {
+				log error;
+			});
+	}
+}
