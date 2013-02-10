1d7fa8ecab84a09eba744920ffa73fbc376c602d
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2013-02-10 19:09:49 +0900

    change release commands

--- src/optimizer.jsx
+++ src/optimizer.jsx
@@ -179,7 +179,7 @@ class Optimizer {
 	var _enableRunTimeTypeCheck : boolean;
 
 	static function getReleaseOptimizationCommands() : string[] {
-		return [ "lto", "no-assert", "no-log", "no-debug", "fold-const", "return-if", "inline", "dce", "unbox", "fold-const", "lcse", "dce", "fold-const", "array-length", "unclassify" ];
+		return [ "lto", "no-assert", "no-log", "no-debug", "non-virtual", "fold-const", "return-if", "inline", "dce", "unbox", "fold-const", "lcse", "dce", "fold-const", "array-length", "unclassify" ];
 	}
 
 	function constructor () {
