c0f6e5aa2c3f303f3a4a346199d0f97c506b50d4
Fuji, Goro <g.psy.va@gmail.com>
2013-03-29 00:38:21 +0900

    do `git push origin master` in `npm publish`

--- tool/postpublish.jsx
+++ tool/postpublish.jsx
@@ -4,39 +4,49 @@ import "console.jsx";
 class _Main {
     static function main(args : string[]) : void {
         var p = node.require(process.cwd() + "/package.json");
-        var version = "v" + p["version"] as string;
+        var tag = "v" + p["version"] as string;
 
-        // git tag VERSION
-        console.log("> git tag " + version);
-        node.child_process.execFile("git", ["tag", version], (err, stdout, stderr) -> {
-            if (err) {
-                console.error(err.toString());
-                process.exit(0);
-            }
+        var dry_run = (args.length > 0 && args[0] == "--dry-run");
 
-            if(stdout) {
-                process.stdout.write(stdout);
-            }
-            if (stderr) {
-                process.stderr.write(stderr);
-            }
+        var tasks = [
+            ["git", "tag", tag],
+            ["git", "push", "origin", tag],
+            ["git", "push", "origin", "master"]
+        ];
 
-            // git push origin VERSION
-            console.log("> git push origin " + version);
-            node.child_process.execFile("git", ["push", "origin", version], (err, stdout, stderr) -> {
-                if (err) {
-                    console.error(err.toString());
-                    process.exit(0);
-                }
-                if(stdout) {
-                    process.stdout.write(stdout);
-                }
-                if (stderr) {
-                    process.stderr.write(stderr);
-                }
+        if (dry_run) {
+            tasks.forEach((item) -> {
+                item.unshift("echo");
             });
-        });
+        }
+
+        function runNext(tasks : string[][]) : void {
+            if (tasks.length) {
+                var task = tasks.shift().slice(0);
+
+                console.log("> " + task.join(" "));
+                var cmd = task.shift();
+                var args = task;
+
+                node.child_process.execFile(cmd, args, (err, stdout, stderr) -> {
+                    if (err) {
+                        console.error(err.toString());
+                        process.exit(0);
+                    }
+                    if(stdout) {
+                        process.stdout.write(stdout);
+                    }
+                    if (stderr) {
+                        process.stderr.write(stderr);
+                    }
+
+                    runNext(tasks);
+                });
+            }
+        }
+
+        runNext(tasks);
     }
 }
 
-// vim: set expandab:
+// vim: set expandtab:
