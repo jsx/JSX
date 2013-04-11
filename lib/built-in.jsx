2b210c72d12bf20ca7bd88627b03d922144a8484
Yuichi Nishiwaki <nishiwaki.yuichi@dena.jp>
2013-04-11 20:26:15 +0900

    add the built-in interface for generator stuff

--- lib/built-in.jsx
+++ lib/built-in.jsx
@@ -983,6 +983,44 @@ native class TypeError extends Error {
 	function constructor(message : string);
 }
 
+/**
+ * Provides classes and interfaces related to generator.
+ */
+class StopIteration extends Error {
+	function constructor() { }
+}
+
+interface Enumerable.<T> {
+	function next () : T;
+}
+
+// only used by JSX compiler
+class __jsx_generator.<T> implements Enumerable.<T> {
+
+	var __next : () -> void;
+	var __value : T;
+	var __end : boolean = false;
+
+	function constructor () { }
+
+	override function next () : T {
+		// FIXME: wasabiz
+		// stop propagation of StopIteration from inner generator to outer one
+		if (! this.__end) {
+			try {
+				this.__next();
+			} catch (e : StopIteration) {
+				this.__end = true;
+				throw e;
+			}
+			return this.__value;
+		} else {
+			throw new StopIteration;
+		}
+	}
+
+}
+
 // 5.12
 /**
  * <p>Provides static functions to manipulate JSON.</p>
