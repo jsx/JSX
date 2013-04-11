bc4eaf663f27a15a3023902c4f81d06a8395053c
Yuichi Nishiwaki <nishiwaki.yuichi@dena.jp>
2013-04-11 20:39:02 +0900

    fix completion tests since two new built-in classes were added

--- t/complete/003.ref-to-class.jsx
+++ t/complete/003.ref-to-class.jsx
@@ -82,6 +82,9 @@ class A {
       "word" : "TypeError"
    },
    {
+      "word" : "StopIteration"
+   },
+   {
       "word" : "JSON"
    },
    {
@@ -92,6 +95,9 @@ class A {
    },
    {
       "word" : "Array"
+   },
+   {
+      "word" : "Enumerable"
    },
    {
       "word" : "++"
