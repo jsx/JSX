8c42ef879558ddf873832829ea7ab0dd23cb15d7
Yuichi Nishiwaki <nishiwaki.yuichi@dena.jp>
2013-04-12 22:42:28 +0900

    add some tests

---
+++ t/generator/013.try.todo.jsx
@@ -0,0 +1,32 @@
+/*EXPECTED
+1
+2
+3
+4
+*/
+class _Main {
+	function bar () : Enumerable.<number> {
+		yield 1;
+		yield 2;
+	}
+	function foo () : Enumerable.<number> {
+		var b = bar();
+		try {
+			while (true) {
+				yield b.next();
+			}
+		} catch (e : StopIteration) {
+			// pass
+		}
+		yield 3;
+		yield 4;
+	}
+
+	static function main (args : string[]) : void {
+		var g = (new _Main).foo();
+		log g.next();
+		log g.next();
+		log g.next();
+		log g.next();
+	}
+}
