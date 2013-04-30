698d92d81f5ed0f8d0bdcf1432b12261997dddc7
Fuji, Goro <g.psy.va@gmail.com>
2013-04-30 20:58:03 +0900

    fix tests for "unclassify"
    Because _Main is __export__ by default, _Main won't be unclassified.

--- t/optimize/034.unclassify-nested-rewrite.jsx
+++ t/optimize/034.unclassify-nested-rewrite.jsx
@@ -4,12 +4,19 @@
 /*JSX_OPTS
 --optimize lto,unclassify
 */
-class _Main {
+
+class Foo {
 	var n = 3;
-	function f() : _Main {
+	function f() : Foo {
 		return this;
 	}
 	static function main(args : string[]) : void {
-		log (new _Main).f().f().n;
+		log (new Foo).f().f().n;
+	}
+}
+
+class _Main {
+	static function main(args : string[]) : void {
+		log (new Foo).f().f().n;
 	}
 }
