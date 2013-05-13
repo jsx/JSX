f191ad88bbd9e5f00dab05efe587886a43435357
Yuichi Nishiwaki <nishiwaki.yuichi@dena.jp>
2013-05-13 23:07:06 +0900

    add a test

---
+++ t/optimize/047.staticize-super-in-staticizable.jsx
@@ -0,0 +1,18 @@
+/*JSX_OPTS
+--optimize staticize,inline
+*/
+class Base {
+	final function foo () : void {
+		this;
+	}
+}
+class Derived extends Base {
+	final function bar () : void {
+		super.foo();
+	}
+}
+class _Main {
+	static function main (args : string[]) : void {
+		(new Derived).bar();
+	}
+}
