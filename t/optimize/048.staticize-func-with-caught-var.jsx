46556f3184ed49691cfacc0c5d58207021a0fbb1
Yuichi Nishiwaki <nishiwaki.yuichi@dena.jp>
2013-05-14 20:26:08 +0900

    add a test

---
+++ t/optimize/048.staticize-func-with-caught-var.jsx
@@ -0,0 +1,22 @@
+/*EXPECTED
+2
+*/
+/*JSX_OPTS
+--optimize staticize --minify
+*/
+import "console.jsx";
+
+class _Main {
+	final function foo () : void {
+		function bar () : void {
+			try {
+				log 1 + 1;
+			} catch (e : Error) {
+				log e.stack;
+			}
+		}
+	}
+	static function main (args : string[]) : void {
+		(new _Main).foo();
+	}
+}
