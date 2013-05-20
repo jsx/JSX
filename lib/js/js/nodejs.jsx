9fcab90d1f06eb8b1f92db87ae5d36d68d3b836f
Yuichi Nishiwaki <nishiwaki.yuichi@dena.jp>
2013-05-20 15:45:31 +0900

    disable async functions in nodejs library for the time being

--- lib/js/js/nodejs.jsx
+++ lib/js/js/nodejs.jsx
@@ -278,8 +278,9 @@ native __fake__ class _fs {
 	function readSync(fd : int, buffer : Buffer, offset : int, length : int, position : int) : int;
 	function readFileSync(filename : string) : Buffer;
 	function readFileSync(filename : string, encoding : string) : string;
-	async function readFileAsync(filename : string) : AsyncTask.<Buffer>;
-	async function readFileAsync(filename : string, encoding : string) : AsyncTask.<string>;
+	// NOTE disabled for the time being
+	// async function readFileAsync(filename : string) : AsyncTask.<Buffer>;
+	// async function readFileAsync(filename : string, encoding : string) : AsyncTask.<string>;
 
 	function writeSync(fd : int, buffer : Buffer, offset : int, length : int) : int;
 	function writeSync(fd : int, buffer : Buffer, offset : int, length : int, position : int) : int;
