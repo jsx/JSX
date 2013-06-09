a85c61310c4b3b3ab7735faac1f107536361c022
Fuji, Goro <g.psy.va@gmail.com>
2013-06-09 00:10:19 -0700

    fix a TODO test for #115

---
+++ t/run/286.issue-115.todo.jsx
@@ -0,0 +1,31 @@
+/*EXPECTED
+a   x
+a   y
+a   z
+b   x
+b   y
+b   z
+c   x
+c   y
+c   z
+3
+6
+*/
+
+class _Main {
+	static function main(args : string[]) : void {
+		var s = 0;
+		var t = 0;
+		var a = [ "a", "b", "c" ];
+		var b = [ "x", "y", "z" ];
+		for (var i in a) {
+			s += i;
+			for (var j in b) {
+				log a[i], " ", b[j];
+				t += j;
+			}
+		}
+		log s; // 0 + 1 + 2
+		log t; // (0 + 1 + 2) * 3
+	}
+}
