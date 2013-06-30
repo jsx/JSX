a8187797fda3d951689578ca3aa0ced5991a9bf0
Fuji, Goro <g.psy.va@gmail.com>
2013-06-30 16:16:47 -0700

    add a TODO test

---
+++ t/run/306.multi-ind/a.jsx
@@ -0,0 +1,6 @@
+native class Foo {
+  static var name : string;
+} = "{ name: 'b.jsx' }";
+
+// vim: set tabstop=2 shiftwidth=2 expandtab:
+
---
+++ t/run/306.multi-ind/b.jsx
@@ -0,0 +1,6 @@
+native class Foo {
+  static var name : string;
+} = "{ name: 'a.jsx' }";
+
+// vim: set tabstop=2 shiftwidth=2 expandtab:
+
