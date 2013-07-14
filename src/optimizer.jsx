0b83d85db49e44bb76b695a0cbf7e80a8e1f9db6
Fuji, Goro <g.psy.va@gmail.com>
2013-07-13 19:59:28 -0700

    [inline] do not declare exta vars

--- src/optimizer.jsx
+++ src/optimizer.jsx
@@ -2661,11 +2661,32 @@ class _InlineOptimizeCommand extends _FunctionOptimizeCommand {
 					}
 
 					// setup args (arg0 = arg0expr, arg1 = arg1expr, ...)
+					//   for non-leaf expressions used more than once
+					var localUsed = new Map.<number>;
+					expr.forEachExpression((expr) -> {
+						if (expr instanceof LocalExpression) {
+							localUsed[(expr as LocalExpression).getLocal().getName().getValue()]++;
+						}
+						else if (expr instanceof ThisExpression) {
+							localUsed["this"]++;
+						}
+						return true;
+					});
 					var setupArgs = null : Expression;
 					for (var i = 0; i < args.length; ++i) {
 						if (args[i] instanceof LeafExpression || args[i] == null) {
 							continue;
 						}
+
+						// use the expression as is if it used only once
+						var usedCount = i < callingFuncDef.getArguments().length
+							? localUsed[callingFuncDef.getArguments()[i].getName().getValue()]
+							: localUsed["this"];
+						if (usedCount == null ||  usedCount == 1) {
+							 // no need to save it to local var
+							continue;
+						}
+
 						var argDecl = callingFuncDef.getArguments()[i];
 						var local = this.createVar(
 								funcDef,
