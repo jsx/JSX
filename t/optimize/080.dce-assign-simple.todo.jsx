7ef89e805117d6d6446cbac2c6f95f369e33d2ff
Fuji, Goro <g.psy.va@gmail.com>
2013-07-14 20:39:40 -0700

    add a todo test

---
+++ t/optimize/080.dce-assign-simple.todo.jsx
@@ -0,0 +1,14 @@
+/*EXPECTED
+15
+*/
+/*JSX_OPTS
+--optimize dce
+*/
+
+class _Main {
+	static function main(args : string[]) : void {
+		// should be converted into `log 1 + ((10 + 2) + 2`
+		var y = 10 + 2;
+		log 1 + (y + 2);
+	}
+}
