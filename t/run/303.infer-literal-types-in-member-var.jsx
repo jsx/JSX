53b7e01aa380f3dd13772129a3d1862fb39914db
Fuji, Goro <g.psy.va@gmail.com>
2013-08-17 11:43:09 +0900

    resolve a TODO test: t/run/303

--- t/run/303.infer-literal-types-in-member-var.jsx
+++ t/run/303.infer-literal-types-in-member-var.jsx
@@ -1,12 +1,16 @@
 /*EXPECTED
 ok
+0
+0
 */
 class A {
 }
 class _Main {
-	var a : A[]	= [];
+	var a : A[] = [];
 	var b : Map.<A> = {};
 	static function main (args : string[]) : void {
 		log "ok";
+		log A.length;
+		log b.keys().length;
 	}
 }
