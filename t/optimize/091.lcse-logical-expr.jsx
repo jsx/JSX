4b84bfef6d8832be54595055693d03ea127c4a77
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2013-08-18 16:53:04 +0900

    lcse should not optimize expressions inside a logical expr

---
+++ t/optimize/091.lcse-logical-expr.jsx
@@ -0,0 +1,24 @@
+/*JSX_OPTS
+--optimize lcse
+*/
+/*EXPECTED
+ok
+*/
+class Box {
+	var value : Nullable.<string> = "OK";
+}
+
+class _Main {
+	static function main (args : string[]) : void {
+
+		function print(x : Nullable.<string>) : boolean {
+			log x;
+			return true;
+		}
+
+		var x = new Box;
+
+		if (false && x.value == null || print(x.value)) {} // should not be `false && (tmp = x.value) == null || print(tmp)`
+
+	}
+}
