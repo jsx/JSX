6709aa737e182da40ee650dfc45093c0a5dbb382
Yuichi Nishiwaki <nishiwaki.yuichi@dena.jp>
2013-08-27 17:54:47 +0900

    remove an unused method

--- src/expression.jsx
+++ src/expression.jsx
@@ -45,21 +45,6 @@ abstract class Expression implements Stashable {
 	abstract function clone () : Expression;
 	abstract function serialize () : variant;
 
-	function deepClone (parent : MemberFunctionDefinition) : Expression {
-		var cloned = this.clone();
-		cloned.forEachExpression (function onExpr (expr) {
-			if (expr instanceof FunctionExpression) {
-				var newFuncDef = (expr as FunctionExpression).getFuncDef().clone();
-				Util.unlinkFunction(newFuncDef, (expr as FunctionExpression).getFuncDef().getParent());
-				Util.linkFunction(newFuncDef, parent);
-				(expr as FunctionExpression).setFuncDef(newFuncDef);
-				return true;
-			}
-			return expr.forEachExpression(onExpr);
-		});
-		return cloned;
-	}
-
 	function instantiate (instantiationContext : InstantiationContext) : boolean {
 		function onExpr(expr : Expression) : boolean {
 			if (expr instanceof NullExpression) {
