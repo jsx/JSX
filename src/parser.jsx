2ffc8cb300d795d70e4639ff062be9854feea322
Yuichi Nishiwaki <nishiwaki.yuichi@dena.jp>
2013-12-03 15:21:36 +0900

    accept '=>' anywhere '->' is acccepcted

--- src/parser.jsx
+++ src/parser.jsx
@@ -2125,7 +2125,7 @@ class Parser {
 			} while (token.getValue() == ",");
 		}
 		// parse return type
-		if (this._expect("->") == null)
+		if (this._expect(["->", "=>"]) == null)
 			return null;
 		var returnType = this._typeDeclaration(true);
 		if (returnType == null)
@@ -3120,7 +3120,7 @@ class Parser {
 			if ((returnType = this._typeDeclaration(true)) == null)
 				return null;
 		}
-		if (this._expect("->") == null)
+		if (this._expect(["->", "=>"]) == null)
 			return null;
 		var funcDef = this._functionBody(token, null, null, args, returnType, this._expectOpt("{") != null, false);
 		if (funcDef == null)
