5175eca768224c80cd06ab558c02cfce94323c98
Kazuho Oku <kazuhooku@gmail.com>
2013-12-11 04:38:33 +0900

    add "et al." to the copyright clause of each file, clarify in LICENSE that the copyrights to whom the copyright belongs to

--- src/_doc.jsx
+++ src/_doc.jsx
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 DeNA Co., Ltd.
+ * Copyright (c) 2012,2013 DeNA Co., Ltd. et al.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
