9ef93d4cdc0a51ffd5100df91268b9833b7d352a
Yuichi Nishiwaki <nishiwaki.yuichi@dena.jp>
2013-12-16 21:51:50 +0900

    emit class prototypes

--- src/cxxemitter.jsx
+++ src/cxxemitter.jsx
@@ -65,7 +65,86 @@ class CplusplusEmitter implements Emitter {
 		return false;
 	}
 
+	var _indent = 0;
+
+	function _emit (str : string) : void {
+		for (var i = 0; i < this._indent; ++i) {
+			this._output += "  ";
+		}
+		this._output += str + "\n";
+	}
+
+	function _advanceIndent () : void {
+		this._indent++;
+	}
+
+	function _reduceIndent () : void {
+		this._indent--;
+	}
+
+	var _emittingClass : ClassDefinition = null;
+
 	override function emit (classDefs : ClassDefinition[]) : void {
+		this._output += "#include <gc_cpp.h>\n";
+
+		for (var i = 0; i < classDefs.length; ++i) {
+			if ((classDefs[i].flags() & ClassDefinition.IS_NATIVE) != 0) {
+				continue;
+			}
+			if (classDefs[i] instanceof TemplateClassDefinition || classDefs[i] instanceof InstantiatedClassDefinition) {
+				continue;
+			}
+			this._output += "class " + classDefs[i].className() + " : public " + classDefs[i].extendType().toString() + " {\n";
+			this._emittingClass = classDefs[i];
+			try {
+				this._output += "public:\n";
+				this._advanceIndent();
+				classDefs[i].forEachMemberFunction(function (funcDef) {
+					this._emitMemberFunction(funcDef);
+					return true;
+				});
+				this._reduceIndent();
+
+				this._output += "private:\n";
+				this._advanceIndent();
+				classDefs[i].forEachMemberVariable(function (varDef) {
+					this._emitMemberVariable(varDef);
+					return true;
+				});
+				this._reduceIndent();
+			} finally {
+				this._output += "};\n\n";
+				this._emittingClass = null;
+			}
+		}
+	}
+
+	function _emitMemberFunction (funcDef : MemberFunctionDefinition) : void {
+		var output = "";
+		if (funcDef instanceof TemplateFunctionDefinition) {
+			return;
+		}
+		if (funcDef.name() == "constructor") {
+			output += funcDef.getClassDef().className() + " (";
+		}
+		else {
+			output += funcDef.getReturnType().toString() + " " + funcDef.name() + " (";
+		}
+		for (var i = 0; i < funcDef.getArguments().length; ++i) {
+			var arg = funcDef.getArguments()[i];
+			output += arg.getType().toString();
+			output += " ";
+			output += arg.getName().getValue();
+			if (i + 1 < funcDef.getArguments().length) {
+				output += ", ";
+			}
+		}
+		output += ");";
+		this._emit(output);
+	}
+
+	function _emitMemberVariable (varDef : MemberVariableDefinition) : void {
+		// TODO
 	}
 
 }
