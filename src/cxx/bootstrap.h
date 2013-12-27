c9ffb445cb568932e408718031e6b82f8b873df7
Yuichi Nishiwaki <nishiwaki.yuichi@dena.jp>
2013-12-27 18:42:25 +0900

    cleanup bootstrap.h

--- src/cxx/bootstrap.h
+++ src/cxx/bootstrap.h
@@ -13,77 +13,115 @@ namespace JSX {
 
   class string {
   public:
-    string () : string("") {}
-    string (const char* data) : length_(strlen(data)), data_(data) {}
+
+    string () : string("") {
+    }
+
+    string (const char* data) :
+      length_(strlen(data)),
+      data_(data) {
+    }
+
     string (number n);
-    operator const char* () const { return data_; }
-    string operator + (const string& rhs) const;
+
+    operator const char* () const {
+      return data_;
+    }
+
+    string operator+ (const string& rhs) const;
+
   private:
+
     int length_;
     const char* data_;
+
   };
 
-  string operator + (const char *lhs, const string& rhs);
+  string operator+ (const char *lhs, const string& rhs);
 
-  template<typename T>
-  class Nullable {
+  template<typename T> class Nullable {
   public:
-    Nullable () : isNull_(true) {}
-    Nullable (const T& value) : isNull_(false), value_(value) {}
+
+    Nullable () :
+      isNull_(true) {
+    }
+
+    Nullable (const T& value) :
+      isNull_(false),
+      value_(value) {
+    }
+
     operator T& ();
+
   private:
+
     boolean isNull_;
     T value_;
+
   };
 
   class variant;
 
   class Object {
   public:
+
     virtual string toString () {
       return "[object Object]";
     }
+
   };
 
   class Function : public Object {
   private:
-    Function() {}
+
+    Function() {
+    }
+
   };
 
-  template<typename T>
-  class Array : public Object {
+  template<typename T> class Array : public Object {
   public:
-    Array () : Array(0) {}
-    Array (number length)
-      : length(length)
-      , ary_(length) {
-    }
-    void push(const T& t);
-    Nullable<T> shift();
+
     number length;
 
+    Array () :
+      Array(0) {
+    }
+
+    Array (number length) :
+      length(length),
+      ary_(length) {
+    }
+
+    void push(const T& t);
+
+    Nullable<T> shift();
+
   private:
+
     std::deque<Nullable<T>> ary_;
+
   };
 
   class Error : public Object {
   public:
-    Error ()
-      : name()
-      , message()
-      , stack() {
-    }
-
-    explicit Error (const string& message)
-      : name()
-      , message(message)
-      , stack() {
-    }
 
     string name;
     string message;
-
     string stack;
+
+    Error () :
+      name(),
+      message(),
+      stack() {
+    }
+
+    explicit Error (const string& message) :
+      name(),
+      message(message),
+      stack() {
+    }
+
   };
 
   string::string(number n) {
@@ -94,33 +132,30 @@ namespace JSX {
     data_ = data;
   }
 
-  string string::operator +(const string& rhs) const {
+  string string::operator+ (const string& rhs) const {
     char *data = new char[length_ + rhs.length_];
     strcpy(data, data_);
     strcpy(data + length_, rhs.data_);
     return string(data);
   }
 
-  string operator + (const char *lhs, const string& rhs) {
+  string operator+ (const char *lhs, const string& rhs) {
     return string(lhs) + rhs;
   }
 
-  template<typename T>
-  Nullable<T>::operator T& () {
+  template<typename T> Nullable<T>::operator T& () {
     if (isNull_) {
       throw new Error("null access");
     }
     return value_;
   }
 
-  template<typename T>
-  void Array<T>::push(const T& t) {
+  template<typename T> void Array<T>::push(const T& t) {
     ary_.push_back(t);
     length++;
   }
 
-  template<typename T>
-  Nullable<T> Array<T>::shift() {
+  template<typename T> Nullable<T> Array<T>::shift() {
     Nullable<T> first = ary_[0];
     ary_.pop_front();
     length--;
@@ -129,9 +164,11 @@ namespace JSX {
 
   class console : public Object {
   public:
+
     static void log (const string& str);
     static void log (const number& num);
     static void log (Object *obj);
+
   };
 
   void console::log (const string& str) {
