44668c7ab6fb7d46a8bacd10e8c410d9384d1d74
Kazuho Oku <kazuhooku@gmail.com>
2014-01-17 11:27:10 +0900

    add a failing test-case (const-int % 0 should be folded to NaN)

--- t/run/368.int-fold-const.jsx
+++ t/run/368.int-fold-const.jsx
@@ -4,13 +4,16 @@
 /*EXPECTED
 -2147483648
 560833313
+NaN
 */
 class _Main {
 	static const OX7FFFFFFF : int = 0x7fffffff;
+	static const ZERO : int = 0;
 	static const ONE : int = 1;
 	static const DEADBEEF_INT : int = 0xdeadbeef;
 	static function main(args : string[]) : void {
 		log _Main.OX7FFFFFFF + _Main.ONE;
 		log _Main.DEADBEEF_INT * _Main.DEADBEEF_INT;
+		log _Main.DEADBEEF_INT % _Main.ZERO;
 	}
 }
