74a02c1538d974b94bd0ed9cae4ca4782367335b
Kazuho Oku <kazuhooku@gmail.com>
2014-01-24 18:10:55 +0900

    add testcase for npm-style import (based on pr #275 by shibukawa-san)

---
+++ t/run/365.npm-import.jsx
@@ -0,0 +1,13 @@
+/*JSX_OPTS
+--add-search-path t/run/365.npm-import
+*/
+/*EXPECTED
+*/
+
+import "importer.jsx";
+
+class _Main {
+  static function main(args : string[]) : void {
+  }
+}
+// vim: set expandtab tabstop=2 shiftwidth=2 ft=jsx:
