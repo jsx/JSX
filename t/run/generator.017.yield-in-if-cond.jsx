1d49adf3345c25b7a032ac488587da09bff95714
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-03-05 20:19:40 +0900

    add another test

---
+++ t/run/generator.017.yield-in-if-cond.jsx
@@ -0,0 +1,29 @@
+/*JSX_OPTS
+--enable-generator-emulation
+*/
+/*EXPECTED
+0
+1
+2
+3
+4
+-1
+*/
+class _Main {
+	static function main (args : string[]) : void {
+		function * incr () : number {
+			var i = 0;
+			while ((yield i++) != 0)
+				;
+			yield -1;
+		}
+
+		var g = incr();
+		log g.next().value;
+		log g.next().value;
+		log g.next().value;
+		log g.next().value;
+		log g.next().value;
+		log g.next(0).value;	// stop
+	}
+}
