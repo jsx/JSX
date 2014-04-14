c378a13b799a10345e7d0f39d8ecd64576151412
Kazuho Oku <kazuhooku@gmail.com>
2014-04-14 14:12:29 +0900

    test should cover the use of: "{ ... } : variant"

--- t/run/393.overflow-in-complit-of-int.jsx
+++ t/run/393.overflow-in-complit-of-int.jsx
@@ -1,6 +1,7 @@
 /*EXPECTED
 -1
 -1
+4294967295
 */
 class _Main {
 	static function main(args : string[]) : void {
@@ -8,5 +9,7 @@ class _Main {
 		log a[0];
 		var m = { k: 0xffffffff } : Map.<int>;
 		log m["k"];
+		var v = { k : 0xffffffff } : variant;
+		log v["k"];
 	}
 }
