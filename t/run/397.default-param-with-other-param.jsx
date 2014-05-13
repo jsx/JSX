c14f22468d885c7be0b85752c20a5621af90074e
Kazuho Oku <kazuhooku@gmail.com>
2014-05-13 18:09:43 +0900

    add test with side-effect

--- t/run/397.default-param-with-other-param.jsx
+++ t/run/397.default-param-with-other-param.jsx
@@ -5,6 +5,9 @@ f
 f
 10
 20
+f
+1
+43
 */
 class _Main {
   static function f(a : number, b : number = a + 42) : void {
@@ -16,6 +19,8 @@ class _Main {
   static function main(args : string[]) : void {
     _Main.f(1);
     _Main.f(10, 20);
+    var i = 1;
+    _Main.f(i++);
   }
 }
 // vim: set expandtab tabstop=2 shiftwidth=2 ft=jsx:
